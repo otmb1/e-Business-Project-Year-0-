56a939998a49dad6be848b9fa818da7969974af5
Ore Deru <93195185+otmb1@users.noreply.github.com>
2023-02-01 01:12:36 +0000

    Add files via upload

---
+++ e-business Website/script/scripts.js
@@ -0,0 +1,44 @@
+$(document).ready(function(){
+	
+	$('#btn_personal_details').click(function(){
+	var error_first_name = '';
+	var error_last_name = '';
+	
+	if($.trim($('#first_name').val()).length == 0)
+  {
+   error_first_name = 'First Name is required';
+   $('#error_first_name').text(error_first_name);
+   $('#first_name').addClass('has-error');
+  }
+  else
+  {
+   error_first_name = '';
+   $('#error_first_name').text(error_first_name);
+   $('#first_name').removeClass('has-error');
+  }
+
+  if($.trim($('#last_name').val()).length == 0)
+  {
+   error_last_name = 'Last Name is required';
+   $('#error_last_name').text(error_last_name);
+   $('#last_name').addClass('has-error');
+  }
+  else
+  {
+   error_last_name = '';
+   $('#error_last_name').text(error_last_name);
+   $('#last_name').removeClass('has-error');
+  }
+
+
+  $('.color-choose input').on('click', function() {
+      var headphonesColor = $(this).attr('data-image');
+
+      $('.active').removeClass('active');
+      $('.left-column img[data-image = ' + headphonesColor + ']').addClass('active');
+      $(this).addClass('active');
+  });
+
+
+
+});
